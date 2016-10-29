988770587b44076ab4ba58611ee8035cdf2d4557
Michael Wornow <mwornow98@gmail.com>
2016-10-29 16:54:34 -0400

    Asst 3

---
+++ asst/3/newspaper/content/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Content(models.Model):
+    title = models.CharField(max_length=500)
+    subtitle = models.CharField(max_length=500)
+    contributors = models.ManyToManyField('Contributor',
+                                          related_name='content')
+    pub_date = models.DateTimeField('date published')
+
+
+class Article(Content):
+    text = models.TextField
+
+    def show():
+    	return title+": "+subtitle+", By "+contributors+". Published "+pub_date
+
+
+class Contributor(models.Model):
+	first_name = models.CharField(max_length=500)
+	last_name = models.CharField(max_length=500)
+
+	def die():
+		models.delete()
